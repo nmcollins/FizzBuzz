0f93263ebfd0374aad125766b48f853c3044c176
nmcollins <NMcollins-cc@conestogac.on.ca>
2016-11-17 17:11:05 -0500

    Initial Set up
    Upload solution done in Visual Studio, C#

---
+++ FizzBuzz/FizzBuzz.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace FizzBuzz
+{
+    partial class FizzBuzz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBxNumbers = new System.Windows.Forms.TextBox();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBxNumbers
+            // 
+            this.txtBxNumbers.Location = new System.Drawing.Point(13, 67);
+            this.txtBxNumbers.Multiline = true;
+            this.txtBxNumbers.Name = "txtBxNumbers";
+            this.txtBxNumbers.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtBxNumbers.Size = new System.Drawing.Size(355, 425);
+            this.txtBxNumbers.TabIndex = 0;
+            this.txtBxNumbers.TabStop = false;
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(13, 26);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(291, 26);
+            this.lblDescription.TabIndex = 1;
+            this.lblDescription.Text = "Display numbers from 1 to 100, show Buzz for multiples of 3, \r\nFizz for multiples" +
+    " of 5 and FizzBuzz for multiples of both";
+            // 
+            // FizzBuzz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 514);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.txtBxNumbers);
+            this.Name = "FizzBuzz";
+            this.Text = "FizzBuzz";
+            this.Load += new System.EventHandler(this.FizzBuzz_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBxNumbers;
+        private System.Windows.Forms.Label lblDescription;
+    }
+}
+
---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FizzBuzz
+{
+    public partial class FizzBuzz : Form
+    {
+        public FizzBuzz()
+        {
+            InitializeComponent();
+        }
+
+        private void FizzBuzz_Load(object sender, EventArgs e)
+        {
+            //Loop through integers from 1 to 100
+            for (int i=1; i < 101; i++)
+            {
+                //Check for multiples of both 3 and 5 first
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    txtBxNumbers.Text += "FizzBuzz";
+                }
+                //Otherwise, check for each separately
+                else if (i % 3 == 0)
+                {
+                    txtBxNumbers.Text += "Buzz";
+                }
+                else if (i % 5 == 0)
+                {
+                    txtBxNumbers.Text += "Fizz";
+                }
+                else
+                {
+                    txtBxNumbers.Text += i.ToString();
+                }     
+                //A lilne return makes the output more easily readable           
+                txtBxNumbers.Text += "\r\n";  
+            }
+
+        }
+    }
+}
